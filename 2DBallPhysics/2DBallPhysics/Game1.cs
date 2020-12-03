b28facea5f485e8f7196f8489bfe136e5190084f
Joakim Ekbladh <joakim.ekbladh@gmail.com>
2020-12-03 14:28:02 +0100

    init

---
+++ 2DBallPhysics/2DBallPhysics/Game1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace _2DBallPhysics
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphicsDevice;
+        private SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphicsDevice = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            
+        }
+    }
+}
